5a8cc8423ae5d28ada86b5e016b81d0906f6ba56
Rogerio <Rogerio@192.168.1.5>
2016-11-21 23:01:08 -0300

    Finalizando CruD

--- contasSpring/src/br/com/caelum/contas/controller/ContaController.java
+++ contasSpring/src/br/com/caelum/contas/controller/ContaController.java
@@ -3,8 +3,9 @@ package br.com.caelum.contas.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.contas.dao.ContaDAO;
 import br.com.caelum.contas.modelo.Conta;
@@ -24,12 +25,26 @@ public class ContaController {
 		return "conta/conta-adicionada";
 	}
 	
-	@RequestMapping(value="/listarContas")
+	@RequestMapping("/listaContas")
 	public ModelAndView listar(){
 		ContaDAO dao = new ContaDAO();
 		List<Conta> contas = dao.lista();
 		ModelAndView mv = new ModelAndView("conta/lista");
 		mv.addObject("contas", contas);
 		return mv;
+	}
+	
+	@RequestMapping("/removeConta")
+	public String remove(Conta conta){
+		ContaDAO dao = new ContaDAO();
+		dao.remove(conta);
+		return "forward:listaContas";
+	}
+	
+	@RequestMapping("/mostrarConta")
+	public String mostra(Long id, Model model){
+		ContaDAO dao = new ContaDAO();
+		model.addAttribute("conta", dao.buscaPorId(id));
+		return "conta/mostra";
 	}
 }
